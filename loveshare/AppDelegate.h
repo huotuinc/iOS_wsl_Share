ea13507a26d82804033990968676206b8e4ee32b
sxluohaibo@163.com <605455337@qq.com>
2016-03-04 11:35:13 +0800

    Initial Commit

---
+++ loveshare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  loveshare
+//
+//  Created by lhb on 16/3/4.
+//  Copyright © 2016年 HT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ loveshare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  loveshare
+//
+//  Created by lhb on 16/3/4.
+//  Copyright © 2016年 HT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
